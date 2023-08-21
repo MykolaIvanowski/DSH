b4986f4e185f10c9712ccea382ef35126677ef5d
mykola <mykolaivanowski@mail.com>
2023-08-21 22:28:03 +0300

    commit

--- dsh/urls.py
+++ dsh/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("polls/", include("polls.urls")),
+    path("index/", include("app_dsh.urls")),
+    path("", include("app_dsh.urls")),
 ]
