5563face96545623b30c5e9e2f77180274adce2f
mykola <mykolaivanowski@mail.com>
2023-09-08 00:01:59 +0300

    run pre commit auto update

--- app_dsh/apps.py
+++ app_dsh/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class PollsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app_dsh'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "app_dsh"
