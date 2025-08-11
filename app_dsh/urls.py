586ff9338f19b8a5dce927388c410da22c85503f
mykola <mykolaivanowski@gmail.com>
2025-08-11 18:16:45 +0100

    product detail

--- app_dsh/urls.py
+++ app_dsh/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path("home/", views.home, name="home"),
     path("login/", views.login_user, name='login'),
     path("logout/", views.logout_user, name='logout'),
-    path("register/", views.register_user, name='register')
+    path("register/", views.register_user, name='register'),
+    path("product/<int:id>", views.product_detail, name='product_detail')
 ]
