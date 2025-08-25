56d6c3c81e0073d29d937dead5f4d51bf4732837
mykola <mykolaivanowski@gmail.com>
2025-08-25 18:34:56 +0100

    payment app

---
+++ dsh_payment/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class DeliveryAddress(models.Model):
+    pass
+
+class Order(models.Model):
+    pass
+
+class OrderItem(models.Model):
+    pass
