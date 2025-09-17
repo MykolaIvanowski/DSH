7b5caf34c0553af9b8431538feb5d8efb19fc81d
mykola <mykolaivanowski@gmail.com>
2025-09-17 23:51:33 +0100

    view detail

--- dsh/urls.py
+++ dsh/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("index/", include("app_dsh.urls")),
     path("", include("app_dsh.urls")),
+    path('cart/', include("cart.urls"))
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
