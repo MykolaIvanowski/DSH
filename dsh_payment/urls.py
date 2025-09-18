35141110a545ef53254e24c5bb2984ea39b019b3
mykola <mykolaivanowski@gmail.com>
2025-09-18 15:48:19 +0100

    add button to cart fix

--- dsh_payment/urls.py
+++ dsh_payment/urls.py
@@ -2,13 +2,13 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('billing_info', views.billing_info, 'billing_info'),
-    path('checkout', views.checkout, 'checkout'),
-    path('delivered_dashboard', views.delivered_dash, 'delivered_dashboard'),
-    path('no_delivered_dashboard', views.not_delivered_dash, 'no_delivered_dashboard'),
+    path('billing_info', views.billing_info, name='billing_info'),
+    path('checkout', views.checkout, name='checkout'),
+    path('delivered_dashboard', views.delivered_dash, name='delivered_dashboard'),
+    path('no_delivered_dashboard', views.not_delivered_dash, name='no_delivered_dashboard'),
     path('orders/<int:pk>', views.orders, name='orders'),
     path('payment_failed', views.payment_failed, name='payment_failed'),
     path('payment_success', views.payment_success, name='payment_success'),
-    path('process_order', views.process_order, 'process_order',
-    path('paypal/webhook/', views.paypal_webhook, name='paypal_webhook'),)
+    path('process_order', views.process_order, name='process_order'),
+    path('paypal/webhook/', views.paypal_webhook, name='paypal_webhook'),
 ]
