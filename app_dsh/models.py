b2d7c60149b7b4f90ebb4d0465d5f11996211a85
mykola <mykolaivanowski@gmail.com>
2025-10-17 16:17:48 +0100

    update OrderLog

--- app_dsh/models.py
+++ app_dsh/models.py
@@ -11,20 +11,14 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = 'categories'
 
-class Customer(models.Model):
-    name = models.CharField(max_length=20)
-    phone_number = models.CharField(max_length=15, blank=True, null=True)
-    address = models.TextField(blank=True, null=True)
-
-    def __str__(self):
-        return self.name
 
 class Product(models.Model):
     name = models.CharField(max_length = 255)
     description = models.TextField()
     article = models.CharField(max_length=30, unique=True,blank=True,null=True)
     price = models.DecimalField(max_digits = 10, decimal_places=2)
-    stock = models.PositiveIntegerField()
+    stock = models.PositiveIntegerField(default=0)
+    allow_backorder = models.BooleanField(default=False)
     image = models.ImageField(upload_to='product_images/', blank=True, null=True)
     is_sale = models.BooleanField(default=False)
     sale_price  = models.DecimalField(default=0, decimal_places=2, max_digits=6)
@@ -32,24 +26,4 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
-
-
-class Order(models.Model):
-    STATUS_CHOICES=[
-        ('pending', 'pending'),
-        ('approved', 'approved'),
-        ('shipped', 'shipped'),
-        ('delivered', 'delivered'),
-        ('cancelled', 'cancelled')
-    ]
-
-    user = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(default=1)
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
-    order_date = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f'Order{self.id} by {self.user.name}'
-
 
