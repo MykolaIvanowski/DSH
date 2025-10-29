5130f7ec9fcab07c436de5260c4f72d03c951e6c
mykola <mykolaivanowski@gmail.com>
2025-10-29 14:38:44 +0000

    update settings

--- dsh/settings.py
+++ dsh/settings.py
@@ -108,8 +108,8 @@ if ENV == 'prod':
     AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-    AWS_S3_ENDPOINT_URL = 'https://s3.eu-central-2.wasabisys.com'
-    AWS_S3_REGION_NAME = 'eu-central-2'
+    AWS_S3_ENDPOINT_URL = 'https://s3.eu-central-1.wasabisys.com'
+    AWS_S3_REGION_NAME = 'eu-central-1'
     AWS_QUERYSTRING_AUTH = False
 
 else:
