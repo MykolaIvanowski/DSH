d9aebcff352fb7e17b1200be646347d0c73c9f8c
mykola <mykolaivanowski@gmail.com>
2025-10-30 13:42:31 +0000

    update settings

--- app_dsh/admin.py
+++ app_dsh/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Category, Product
 
 # Register your models here.
 
 admin.site.register(Category)
-admin.site.register(Product)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display =  ['name', 'image_preview', 'image_url']
+
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" width="100" />', obj.image.url)
+    image_preview.short_description = 'Preview'
+
+    def image_url(self, obj):
+        if obj.image:
+            return format_html('<a href="{0}" target="_blank">{0}</a>', obj.image.url)
+
+    image_url.short_description = 'Image url'
