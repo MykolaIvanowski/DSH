6985c6d70c2364e0d2dd0e549e6f3b0bb94e8a09
mykola <mykolaivanowski@gmail.com>
2025-10-31 18:24:21 +0000

    guni set

--- start.sh
+++ start.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-python manage.py migrate
+# Зapply migration
+python manage.py migrate --noinput
+
+# collect static
 python manage.py collectstatic --noinput
 
-gunicorn dsh.wsgi:application  &
+# start Gunicorn
+gunicorn dsh.wsgi:application --bind 0.0.0.0:8000 &
 sleep 2
+# start nginx
+echo "🌐 Starting nginx..."
 nginx -g "daemon off;"
