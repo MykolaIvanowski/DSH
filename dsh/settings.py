c8bc2985b7e263bcd83f8f4af4a955fd37ec6bb4
mykola <mykolaivanowski@gmail.com>
2025-10-31 12:54:41 +0000

    settings

--- dsh/settings.py
+++ dsh/settings.py
@@ -116,7 +116,7 @@ if ENV == 'prod':
     # MEDIA_URL = f'https://s3.eu-central-1.wasabisys.com/{AWS_STORAGE_BUCKET_NAME}/'
     DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
     MEDIA_URL = '/media/'
-    MEDIA_ROOT = '/data/media'
+    MEDIA_ROOT = '/data/media/'
 
 else:
     DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
